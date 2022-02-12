a7948890c10dd5ad7413132fa394dfabf8b5320b
John Foley <johnf@middlebury.edu>
2022-02-12 17:56:17 -0500

    create Welcome.java for lab0

---
+++ src/hw00/Welcome.java
@@ -0,0 +1,14 @@
+package hw00;
+
+/**
+ * Every Java file must have a class defined in it!
+ * Notice that the name of this file is Welcome.java
+ * .. and we define class Welcome!
+ * .. that's super important to Java. It's picky like that.
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Congratulations!");
+        System.out.println("You've installed Java!");
+    }
+}
